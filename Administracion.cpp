128d523546e918ea85245b08ef15e3bdff949fe2
Jaz47 <69018403+Jaz47@users.noreply.github.com>
2020-12-08 19:04:33 -0300

    agregue libreria

--- Administracion.cpp
+++ Administracion.cpp
@@ -1,44 +1,9 @@
 #include<stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-struct fecha{
-	int dia, mes, anio;	
-};
+#include"calculos.h"
 
 
-
-struct Usuarios{
-	char usuario[10];
-	char contras[10];   
-	char ape_y_nomb[60];
-};
-
-struct Veterinario{
-    char ApellidoyNombre[60];
-    int Matricula; 
-    int DNI;
-    char Telefono[25];
-};
-
-struct Mascota{
-	char Apellido_y_Nombre[60];
-    char Domicilio [60];
-    int DNI_Duenio ;
-    char Localidad[60];
-    fecha Fecha_de_Nac;
-    float Peso; 
-    char Telefono[25];
-};
-
-typedef char cadena[380];
-
-struct Turnos{
-	int Matricula_de_Veterinario;
-    fecha Fecha ;
-    int DNI_Duenio; 
-    cadena Detalle_de_Atencion[380];
-};
 
 void reg_veter();
 
