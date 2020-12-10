6f7364c82257784c5a5604d13c0db23c53314a8f
Jaz47 <69018403+Jaz47@users.noreply.github.com>
2020-12-10 17:37:17 -0300

    atencion vet

--- Administracion.cpp
+++ Administracion.cpp
@@ -4,26 +4,33 @@
 #include"calculos.h"
 
 
-
 void reg_veter();
+void aten_vet();
 
 main(){
+	
+	FILE *Arch_Veter, *Arch_Masc, *Arch_Turns;
+	
 	int Opc;
 	do{
 		 printf("\t-----------------------------------------------\n");
-     printf("\t|      **Modulo de Administracion**           |\n");
-     printf("\t| 1-> Registrar Veterinario                   |\n");
-     printf("\t| 2-> Registrar Usuario Asistente             |\n");
-     printf("\t| 3-> Atencion por Veterinaria                |\n");
-     printf("\t| 4-> Ranking de Veterinario por Atencion     |\n");
-     printf("\t| 5-> Cerrar Aplicacion                       |\n");
-     printf("\t-----------------------------------------------\n");
+         printf("\t|      **Modulo de Administracion**           |\n");
+         printf("\t| 1-> Registrar Veterinario                   |\n");
+         printf("\t| 2-> Registrar Usuario Asistente             |\n");
+         printf("\t| 3-> Atencion por Veterinaria                |\n");
+         printf("\t| 4-> Ranking de Veterinario por Atencion     |\n");
+         printf("\t| 5-> Cerrar Aplicacion                       |\n");
+         printf("\t-----------------------------------------------\n");
    
    
    printf("\nIngresar Opcion: ");
-   scanf("%d", & Opc);
+   scanf("%d", &Opc);
    
    switch (Opc){
+   	case 1:
+   	case 2:
+   	case 3:
+   	case 4:
    	
    }
    
@@ -33,7 +40,7 @@ main(){
 
 void reg_veter(){
 	Veterinario vet;
-	FILE *Arch_Veter;
+	
 	
 	Arch_Veter=fopen("Veterinarios.dat", "a+b");
 	
@@ -55,4 +62,38 @@ void reg_veter(){
 		
 	fwrite(&vet, sizeof(Veterinario),1,v);
 	fclose(Arch_Veter);
+}
+
+void aten_vet(){
+	Veterinario Veter;
+	Mascota masc;
+	Turnos tur;
+	cadena apynom;
+	
+	_flushall();
+	printf("Apellido y Nombre de la mascota a atender: ");
+	gets(apynom);
+	Arch_Turns=fopen("Turnos.dat", "r+b");
+	Arch_Masc=fopen("Mascotas.dat", "r+b");
+	
+	while(!feof(Arch_Masc)){
+		if(strcmp(apynom,masc.Apellido_y_Nombre)==0){
+			while(!feof(Arch_Turns)){
+				if(masc.DNI_Duenio==tur.DNI_Duenio){
+					rewind(Arch_Masc);
+					printf("\nDetalle de evolucion de la mascota:\n ");
+					gets(tur.Detalle_de_Atencion);
+					tur.borrado=true;
+					fseek(Arch_Masc, sizeof(Turnos), SEEK_CUP);
+					fwrite(&masc,sizeof(Mascota),1, Arch_Masc);
+					printf("El Registro se dio de baja\n\n");
+				}
+				fread(&tur,sizeof(Turnos),1,Arch_Turns);
+			}
+		}
+		fread(&masc,sizeof(Mascota),1,Arch_Masc);
+	}
+	fclose(Arch_Masc);
+	fclose(Arch_Turns);
+	
 }
