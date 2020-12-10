e56b4daba68352b88f2a298210b1b7793229135e
Jaz47 <69018403+Jaz47@users.noreply.github.com>
2020-12-10 17:28:29 -0300

    Update calculos.h

--- calculos.h
+++ calculos.h
@@ -3,46 +3,9 @@
 #include <string.h>
 
 void menuPrincipal();
-
-struct fecha{
-	int dia, mes, anio;	
-};
-
-
-
-struct Usuarios{
-	char usuario[10];
-	char contras[10];   
-	char ape_y_nomb[60];
-};
-
-struct Veterinario{
-    char ApellidoyNombre[60];
-    int Matricula; 
-    int DNI;
-    char Telefono[25];
-};
-
-struct Mascota{
-	char Apellido_y_Nombre[60];
-    char Domicilio [60];
-    int DNI_Duenio ;
-    char Localidad[60];
-    fecha Fecha_de_Nac;
-    float Peso; 
-    char Telefono[25];
-};
-
-typedef char cadena[380];
-
-struct Turnos{
-	int Matricula_de_Veterinario;
-    fecha Fecha ;
-    int DNI_Duenio; 
-    cadena Detalle_de_Atencion[380];
-};
-
-
+int Menu_Consultorio();
+int Modul_Asist();
+int Modul_Admin();
 
 void menuPrincipal() {
 system("cls");
@@ -54,4 +17,50 @@ system("cls");
   printf("\t| 4-> Salir                            |\n");
   printf("\t----------------------------------------\n");
  }
+ 
+int Menu_Consultorio(){
+  printf("\t------------------------------------------------------\n");
+  printf("\t|      **Modulo Consultorio Veterinario**            |\n");
+  printf("\t| 1-> Iniciar Sesion                                 |\n");
+  printf("\t| 2-> Visualizar Lista de Espera de Turnos (Informes)|\n");
+  printf("\t| 3-> Registrar Evolucion de la Mascota              |\n")
+  printf("\t| 4-> Cerrar Aplicacion                              |\n");
+  printf("\t------------------------------------------------------\n");
+   
+   int Opc;
+   printf("\nIngresar Opcion: ");
+   scanf("%d", & Opc);
+   return Opc;
+ }
 
+int Modul_Asist(){
+  printf("\t-----------------------------------------------\n");
+  printf("\t|      **Modulo de Asistente**                |\n");
+  printf("\t| 1-> Iniciar Sesion                          |\n");
+  printf("\t| 2-> Registrar Mascota                       |\n");
+  printf("\t| 3-> Registrar Turnos                        |\n")
+  printf("\t| 4-> Listar Atencion pot Veterinario y Fecha |\n");
+  printf("\t| 5-> Cerrar Aplicacion                       |\n");
+  printf("\t-----------------------------------------------\n");
+   
+   int Opc;
+   printf("\nIngresar Opcion: ");
+   scanf("%d", & Opc);
+   return Opc;
+ }
+
+int Modul_Admin(){
+  printf("\t-----------------------------------------------\n");
+  printf("\t|      **Modulo de Administracion**           |\n");
+  printf("\t| 1-> Registrar Veterinario                   |\n");
+  printf("\t| 2-> Registrar Usuario Asistente             |\n");
+  printf("\t| 3-> Atencion por Veterinaria                |\n")
+  printf("\t| 4-> Ranking de Veterinario por Atencion     |\n");
+  printf("\t| 5-> Cerrar Aplicacion                       |\n");
+  printf("\t-----------------------------------------------\n");
+   
+   int Opc;
+   printf("\nIngresar Opcion: ");
+   scanf("%d", & Opc);
+   return Opc;
+ }
