0aeb66b812e5d349e4e798f5db7620883cac0032
ange45 <69015731+ange45@users.noreply.github.com>
2020-12-13 17:05:08 -0300

    Update Administracion.cpp

--- Administracion.cpp
+++ Administracion.cpp
@@ -48,6 +48,25 @@ main(){
    }
    
 	}while(Opc!=5);
+	bool crearRank(){
+	FILE *veterinarios = fopen("veterinarios.dat");
+	Veterinario lectura;
+	int matriculas[100];
+	int cantidades [100];
+	c=0
+	
+	fread(&lectura,sizeof(veterinario),1,veterinarios);
+	while(!feof(veterinarios)){
+		matriculas[c] =lectura.Matricula;
+		cantidades[c] =aten_vet(turnos,lectura,matricula);
+		fread (&lectura, sizeof (veterinario), 1,veterinarios);
+	}
+	
+	
+	burbuja()
+	
+	
+	
 	
 }
 
@@ -207,26 +226,7 @@ void aten_vet(*FILE Veterinarios){
 	}while(b==0);
 }
 
-bool crearRank(){
-	FILE *veterinarios = fopen("veterinarios.dat");
-	Veterinario lectura;
-	int matriculas[100];
-	int cantidades [100];
-	c=0
-	
-	fread(&lectura,sizeof(veterinario),1,veterinarios);
-	while(!feof(veterinarios)){
-		matriculas[c] =lectura.Matricula;
-		cantidades[c] =aten_vet(turnos,lectura,matricula);
-		fread (&lectura, sizeof (veterinario), 1,veterinarios);
-	}
-	
-	
-	burbuja()
-	
-	
-	
-	
+
 }
 
 	
