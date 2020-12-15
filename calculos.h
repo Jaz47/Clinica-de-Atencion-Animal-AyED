2d04d29e96cebac4fa98aa061769ce48cdc45a0e
Jaz47 <69018403+Jaz47@users.noreply.github.com>
2020-12-15 19:23:30 -0300

    cambios en la estructuras
    se realizo cambios en la estructura y se agrego funciones de validaciones de usuario y contrasenia

--- calculos.h
+++ calculos.h
@@ -22,38 +22,142 @@ struct fecha{
 };
 
 
+ypedef char cadena [60];
 
 struct Usuarios{
-	char usuario[10];
-	char contras[10];   
-	char ape_y_nomb[60];
+	cadena usuario;
+	cadena contras;   
+	cadena ape_y_nomb;
 };
 
 struct Veterinario{
-    char ApellidoyNombre[60];
-    char contras[10]; 
-    int Matricula; 
+    cadena ApellidoyNombre;
+    cadena Matricula; 
     int DNI;
-    char Telefono[25];
+    cadena Telefono;
+    cadena Usuario;
+    cadena contr;
 };
 
 struct Mascota{
-    char Apellido_y_Nombre[60];
-    char Domicilio [60];
+	cadena Apellido_y_Nombre;
+    cadena Domicilio ;
     int DNI_Duenio ;
-    char Localidad[60];
+    cadena Localidad;
     fecha Fecha_de_Nac;
     float Peso; 
-    char Telefono[25];
+    cadena Telefono;
 };
 
-typedef char cadena[380];
+
 
 struct Turnos{
-    int Matricula_de_Veterinario;
+	int Matricula_de_Veterinario;
     fecha Fecha ;
     int DNI_Duenio; 
-    char Detalle_de_Atencion[380];
+    cadena Detalle_de_Atencion[380];
     bool borrado;
 };
+
+
+void user(FILE *Arch_Usua, cadena &aux){
+	
+	
+	bool bandera = false;
+	
+	do{
+		int longitud, contadorMayus=0, contadorNum=0;
+		printf("\nIngrese su nombre de usuario: ");
+		gets(aux);
+		longitud = strlen(aux);
+		
+		if (longitud > 5 && longitud < 11)
+		{
+			if (aux[0] >= 'a' && aux[0] <= 'z')
+			{
+				for (int i=0; i<longitud; i++)
+				{
+					if (aux[i] >= 'A' && aux[i] <= 'Z')
+					{
+						contadorMayus++;
+					}
+					else
+					{
+						if (aux[i] >= '0' && aux[i] <= '9')
+						{
+							contadorNum++;
+						}
+					}
+				}
+				if (contadorMayus > 1 && contadorNum < 4)
+				{
+					bandera = true;
+				}
+			}
+		}
+		if(bandera == false){
+			printf("\nEl nombre de usuario ingresado no cumple con alguna de las condiciones dadas.");
+			printf("\nVuelva a intentar.\n\n");
+		}
+	}while(bandera == false);
+}
+
+void password(FILE *Arch_Usua, cadena &password){
+	int Mayus,Minus,Num,Signos,NumConsecutivos,letrasCons;
+	bool Band = false;
+	cadena passAux;
+	
+	do{
+		Mayus = 0, Minus = 0, Num = 0, Signos = 0, NumConsecutivos = 0, letrasCons = 1;
+		
+		printf("\nIngrese su contrasenia: ");
+		gets(passAux);
+		strcpy(password, passAux);
+
+		for(int i=0; i<strlen(passAux); i++)
+		{
+			if (passAux[i] >='A' && passAux[i] <='Z')
+			{
+			Mayus++;		
+			} 
+			else 
+			{
+				if (passAux[i] >='a' && passAux[i] <='z')
+				{
+				 Minus++;	
+				}
+				else
+				{
+					if (passAux[i] >= '0' && passAux[i] <= '9')
+					{
+						Num++;
+						NumConsecutivos++;
+					}
+					else
+					{
+						Signos++;
+					}
+				}
+	 		}
+		}
+		strlwr(passAux);
+		
+		for(int i=0; i<strlen(passAux); i++)
+		{
+			if (passAux[i] >= 'a' && passAux[i] <= 'z')
+			{
+			   if(i>0 && passAux[i-1]>'9' && passAux[i]==passAux[i-1] + 1)
+			   {
+			   		letrasCons++;
+			   } 			
+			}
+		}	
+		if (Mayus >= 1 && Minus >= 1 && Num >= 1 && Signos == 0 && letrasCons != 2 && NumConsecutivos <= 3)
+		{
+			Band = true;
+		}	
+	}while(Band == false);
+	 
+}
+
 
